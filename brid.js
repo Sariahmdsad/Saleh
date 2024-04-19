8af08268b9c689f98b82d2f0f0dff85f229e669f
Sariahmdsad <167486662+Sariahmdsad@users.noreply.github.com>
2024-04-19 23:17:18 +0300

    Add files via upload

---
+++ brid.js
@@ -0,0 +1,10 @@
+
+let salh=document.getElementById("xx")
+let fc = document.getElementById("fc");
+let nn=document.getElementById("oppo")
+let nv=0;
+function sx() {
+  ++nv;
+  fc.play("")
+ nn.innerText =nv;
+}
